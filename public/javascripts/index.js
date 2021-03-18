060a2b5a8f3cba58254435f9d7f03d780cff7034
FluffyCherub <73240956+FluffyCherub@users.noreply.github.com>
2021-03-18 18:00:05 +0000

    chat
    Chat works now for being in same room. Picture sending doesnt yet.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,7 @@
 let name = null;
 let roomNo = null;
 let socket=null;
-
+let chat= io.connect('/chat');
 
 /**
  * called by <body onload>
@@ -14,6 +14,7 @@ function init() {
     document.getElementById('chat_interface').style.display = 'none';
 
     //@todo here is where you should initialise the socket operations as described in teh lectures (room joining, chat message receipt etc.)
+    initChatSocket();
 }
 
 /**
@@ -31,8 +32,9 @@ function generateRoom() {
  * and sends the message via  socket
  */
 function sendChatText() {
-    let chatText = document.getElementById('chat_input').value;
     // @todo send the chat message
+    let chatText = document.getElementById('chat_input').value;
+    chat.emit('chat', roomNo, name, chatText);
 }
 
 /**
@@ -45,6 +47,8 @@ function connectToRoom() {
     let imageUrl= document.getElementById('image_url').value;
     if (!name) name = 'Unknown-' + Math.random();
     //@todo join the room
+    chat.emit('create or join', roomNo, name);
+
     initCanvas(socket, imageUrl);
     hideLoginInterface(roomNo, name);
 }
@@ -76,4 +80,45 @@ function hideLoginInterface(room, userId) {
     document.getElementById('who_you_are').innerHTML= userId;
     document.getElementById('in_room').innerHTML= ' '+room;
 }
+
+/////////////////////////////////////////////////////////////////
+
+
+/**
+ * it initialises the socket for /chat
+ */
+function initChatSocket() {
+    // called when someone joins the room. If it is someone else it notifies the joining of the room
+    chat.on('joined', function (room, userId) {
+        if (userId === name) {
+            // it enters the chat
+            hideLoginInterface(room, userId);
+        } else {
+            // notifies that someone has joined the room
+            writeOnHistory('<b>' + userId + '</b>' + ' joined room ' + room);
+        }
+    });
+    // called when a message is received
+    chat.on('chat', function (room, userId, chatText) {
+        let who = userId
+        if (userId === name) who = 'Me';
+        writeOnHistory('<b>' + who + ':</b> ' + chatText);
+    });
+
+}
+/**
+ * it appends the given html text to the history div
+ * @param text: teh text to append
+ */
+/*
+function writeOnChatHistory(text) {
+    let history = document.getElementById('chat_history');
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = text;
+    history.appendChild(paragraph);
+    document.getElementById('chat_input').value = ''
+
+}
+ */
+
 
--- socket.io/socket-io.js
+++ socket.io/socket-io.js
@@ -1,4 +1,5 @@
 
+/*
 exports.init = function(io) {
   io.sockets.on('connection', function (socket) {
     try {
@@ -6,4 +7,31 @@ exports.init = function(io) {
     } catch (e) {
     }
   });
+}*/
+
+
+
+exports.init = function(io) {
+
+  // the chat namespace
+  const chat = io
+      .of('/chat')
+      .on('connection', function (socket) {
+        try {
+          //it creates or joins a room
+          socket.on('create or join', function (room, userId) {
+            socket.join(room);
+            chat.to(room).emit('joined', room, userId);
+          });
+
+          socket.on('chat', function (room, userId, chatText) {
+            chat.to(room).emit('chat', room, userId, chatText);
+          });
+
+          socket.on('disconnect', function () {
+            console.log('someone disconnected');
+          });
+        } catch (e) {
+        }
+      });
 }
